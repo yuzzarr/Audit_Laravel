3ab3cdcf0bf85e664ab25b9e141101e39f1ab550
yuzzarr <yuzzarrizkym@gmail.com>
2023-06-10 16:26:07 +0700

    Audit

--- app/Models/administrator.php
+++ app/Models/administrator.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class administrator extends Model
+class Administrator extends Model
 {
     use HasFactory;
 
--- app/Models/auditee.php
+++ app/Models/auditee.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class auditee extends Model
+class Auditee extends Model
 {
     use HasFactory;
 
